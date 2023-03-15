99b4ec72fca771864cb927b41bb3bcf43c99bf82
Kai Wedekind <wedekind.kai@googlemail.com>
2023-03-15 08:45:38 +0100

    Change base again

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/the-terminal-dev/',
+  base: '/',
   title: "TheTerminal.dev",
   description: "A VitePress Site",
   themeConfig: {
